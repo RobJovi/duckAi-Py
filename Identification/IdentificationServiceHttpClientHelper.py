adb6304f06ab38f97ea75d198012e92d90c08777
nabilkhalil <duomaster01@gmail.com>
2017-09-29 21:12:23 -0700

    pushed to github

--- Identification/IdentificationServiceHttpClientHelper.py
+++ Identification/IdentificationServiceHttpClientHelper.py
@@ -29,6 +29,10 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+import sys
+
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 or a more recent version is required.")
 
 import http.client
 import urllib.parse
